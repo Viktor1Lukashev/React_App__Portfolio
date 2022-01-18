31d6e292f2f9b98f01b3b018986cd226dcc29cbf
Viktor1Likashev <lukashev-viktor2012@yandex.ru>
2022-01-19 00:47:28 +0300

    First Init application

---
+++ src/components/AnastasiaPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function AnastasiaPage() {
+    return(
+        <h2>Страница Анастасии</h2>
+
+    );
+}
+export default AnastasiaPage;
---
+++ src/components/ViktorPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function ViktorPage() {
+    return(
+        <h2>Страница Виктора</h2>
+
+    );
+}
+export default ViktorPage;
